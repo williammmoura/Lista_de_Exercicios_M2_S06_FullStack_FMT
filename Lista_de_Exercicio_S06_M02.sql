b46552adb514a7124493ad5402ca9fafcbc459d6
William Monte de Moura <williamonte@gmail.com>
2023-06-08 21:53:23 -0300

    Exercicio 04

--- Lista_de_Exercicio_S06_M02.sql
+++ Lista_de_Exercicio_S06_M02.sql
@@ -1,8 +1,21 @@
 --Criar tabela Projetos
 create table Projetos(
-	id int primary key,
+	projetos_id int primary key,
 	nome varchar(100),
 	descricao text,
 	data_inicio date,
 	data_prazo date
 );
+
+select * from Projetos
+
+-- Criar tabela Tarefas
+create table Tarefas(
+	tarefas_id int primary key,
+	nome varchar(100),
+	descricao text,
+	data_inicio date,
+	data_prazo date
+);
+
+select * from Tarefas
